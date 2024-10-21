3cc8d8a557327529a97b306e5b9595476b20ae18
yfdfdfdfd <pysonivy@gmail.com>
2024-10-21 15:43:44 +0900

    login

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -9,80 +9,53 @@ import SupportIcon from './icons/IconSupport.vue'
 
 <template>
   <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
+  <template #icon>
+    <DocumentationIcon />
+  </template>
+  <template #heading>ログイン</template>
+  <template #default>
+    <div>
+      <label for="email">メールアドレス:</label>
+      <input
+        type="email"
+        id="email"
+        placeholder="メールアドレスを入力"
+        style="padding: 8px; margin-top: 10px; width: 100%;"
+      />
+    </div>
+  </template>
+</WelcomeItem>
 
   <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+  <template #icon>
+    <CommunityIcon />
+  </template>
+  <template #heading></template>
+  <template #default>
+    <div>
+      <label for="password">パスワード</label>
+      <input
+        type="password"
+        id="password"
+        placeholder="パスワードを入力"
+        style="padding: 8px; margin-top: 10px; margin-bottom: 10px; width: 100%;"
+      />
+      <div style="margin-top: 10px;">
+        <a href="#" style="color: white; text-decoration: underline;">
+          アカウント新規作成
+        </a>
+      </div>
+      <div style="margin-top: 10px;">
+        <a href="#" style="color: white; text-decoration: underline;">
+          パスワードを忘れた
+        </a>
+      </div>
+      <button
+        style="margin-top: 20px; padding: 10px 20px; width: 100%; background-color: #4CAF50; color: white; border: none; border-radius: 5px; cursor: pointer;"
+      >
+        ログイン
+      </button>
+    </div>
+  </template>
+</WelcomeItem>
 </template>
