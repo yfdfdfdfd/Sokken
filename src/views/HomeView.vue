2c59cd27537834353e4d850d035535ceab7071b8
yfdfdfdfd <pysonivy@gmail.com>
2024-11-13 16:43:39 +0900

    ホームビューのテンプレートとスタイルのインデントを修正

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,25 +4,25 @@ import AppHeader from '../components/AppHeader.vue'
 </script>
 
 <template>
-  <div class="center">
-    <div class="grid">
-      <AppHeader />
-      <main>
-        <TheWelcome />
-      </main>
+    <div class="center">
+        <div class="grid">
+            <AppHeader />
+            <main>
+                <TheWelcome />
+            </main>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
 .center {
-  display: flex;
-  place-items: center;
-  height: 100vh;
+    display: flex;
+    place-items: center;
+    height: 100vh;
 }
 .grid {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  padding: 0 2rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
 }
 </style>
