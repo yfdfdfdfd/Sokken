78ee291a2d272b499197e190edda252d83e6ef72
sudo <zehutahai@gmail.com>
2024-11-21 13:28:09 +0900

    AboutView.vueに各ページの説明セクションを追加

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -9,6 +9,47 @@ import AppHeader from '@/components/AppHeader.vue'
       <main>
         <div class="about">
           <h1>サイト説明</h1>
+
+          <section>
+            <h2>Home</h2>
+            <h3>
+              Homeページでは、問題の出題数の変更、時間設定ができます。
+              出題開始ボタンを押すことで始まります。
+            </h3>
+          </section>
+          <br>
+
+          <section>
+            <h2>About</h2>
+            <h3>
+              Aboutページでは、このサイトの説明を見ることができます。
+            </h3>
+          </section> 
+          <br>
+
+          <section>
+            <h2>Generate</h2>
+            <h3>
+              Generateページでは、問題のジャンル、問題文、選択肢、正解を入力して問題を作成できます。
+            </h3>
+          </section>
+          <br>
+
+          <section>
+            <h2>Quize</h2>
+            <h3>
+              Quizeページでは、問題を解くことができます。
+            </h3>
+          </section>
+          <br>
+
+          <section>
+            <h2>History</h2>
+            <h3>
+              Historyページでは、過去に解いた問題を閲覧できます。
+            </h3>
+          </section>
+          <br>
         </div>
       </main>
     </div>
