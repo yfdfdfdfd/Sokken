958051de257313a71681303ca97683c9c84e5734
haohao237 <saikiharuto015@gmail.com>
2024-12-02 14:00:46 +0900

    削除

--- src/generated/models/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './HTTPValidationError';
-export * from './Question';
-export * from './User';
-export * from './UserCreate';
-export * from './UserLogin';
-export * from './ValidationError';
-export * from './ValidationErrorLocInner';
