2cc5257e254e3aed5ff1d80216f7360b9c660365
haohao237 <saikiharuto015@gmail.com>
2024-12-04 15:35:02 +0900

    削除

--- backend/database/feedback.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-from models import FeedbackTemplateModel
-
-def generate_feedback(tag: str, db: Session):
-    feedback_template = db.query(FeedbackTemplateModel).filter(FeedbackTemplateModel.tag == tag).first()
-
-    if feedback_template:
-        return feedback_template.feedback
-    return f"We recommend reviewing topics related to '{tag}' for improvement."
